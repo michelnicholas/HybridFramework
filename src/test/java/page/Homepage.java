ccf15e5270e84d7a739da61d0a1cf722b141b0bb
Nicholas <zmichen@amazon.com>
2021-12-13 06:49:27 -0500

    added reports folder with result html file to connect the test listener to get the report

--- src/test/java/page/Homepage.java
+++ src/test/java/page/Homepage.java
@@ -6,12 +6,15 @@ import utility.DriverUtil;
 
 public class Homepage extends DriverUtil {
 
+    // All available elements and Urls need for Homepage
+    // All located using By indicator
     private String url = "https://trello.com";
     private By page_banner_text = By.xpath("//h1");
     private By login_button = By.xpath("//a[text() = 'Log in']");
     private By sign_up_page = By.xpath("//a[text() = 'Sign up']");
 
 
+    // Method to check and see if the Homepage is displayed
     public boolean isPageDisplay(){
         driver().get(url);
         WebElement pageBanner = driver().findElement(page_banner_text);
