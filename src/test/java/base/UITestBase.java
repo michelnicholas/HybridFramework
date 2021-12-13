166e61726fcc0103682be13af987f6e0105a3579
Nicholas <zmichen@amazon.com>
2021-12-13 06:18:14 -0500

    added a few more lines of code from another local repo to remote

--- src/test/java/base/UITestBase.java
+++ src/test/java/base/UITestBase.java
@@ -2,6 +2,7 @@ package base;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import page.LoginPage;
 import utility.DriverUtil;
 
@@ -20,6 +21,10 @@ public class UITestBase {
         DriverUtil.close();
     }
 
+    @Test
+    public void helloWorld(){
+        System.out.println("Hello world");
+    }
 
 
 
